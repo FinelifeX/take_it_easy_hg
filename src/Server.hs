699eda51075c9a5ef1f660dfe5a23464dcb71fb5
Lisa <gabitova1358@gmail.com>
2019-12-20 13:45:16 +0300

    fix server

--- src/Server.hs
+++ src/Server.hs
@@ -1,4 +1,3 @@
--- Main server file
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -11,21 +10,27 @@ module Server ( startServer ) where
 import Servant
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
 import Data.Text.Lazy ( Text, pack )
---
---Custom modules import
+import Control.Monad.IO.Class ( liftIO )
+
+-- Custom modules import
 import Datas
-import Middlewares
 import Controllers.NewGameController ( newGame )
+import Controllers.UpdateGridController ( updateGrid, respond )
 import Controllers.GetCellsController ( getCells )
 import Controllers.GetFieldStateController ( getFieldState )
+import Controllers.CountScoreController ( countCurrentScore )
+import Middlewares
 --
 -- Route mapping
 type ServerAPI =
         Get '[JSON] Text
    :<|> "new_game" :> Get '[JSON] Field
    :<|> "get_cells" :> Get '[JSON] [Lines]
+   :<|> "update_grid" :> ReqBody '[JSON] Field :> Post '[JSON] Text
    :<|> "get_field_state" :> Post '[JSON] Field
+   :<|> "get_current_score" :> ReqBody '[JSON] Field :> Post '[JSON] Integer
 --
 -- Route handlers
 serverRoutes :: Server ServerAPI
@@ -33,13 +38,19 @@ serverRoutes =
         home
    :<|> startNewGame
    :<|> return getCells
+   :<|> update
    :<|> fieldState
-  where
+   :<|> countScore
+   where
     home = return "Hello from Home"
     startNewGame = return newGame
+    update field = do
+      liftIO $ updateGrid field
+      return respond
     fieldState = do
-              liftIO $ getFieldState
-          >>= return
+          liftIO $ getFieldState
+      >>= return
+    countScore f = return $ countCurrentScore f
 
 
 -- Server initialization
