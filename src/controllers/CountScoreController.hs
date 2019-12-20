ba4c15a57b568dce867851bf67188d2b457ab1f6
Bulat Murtazin <random.finelife@gmail.com>
2019-12-20 13:39:13 +0300

    = count score

--- src/controllers/CountScoreController.hs
+++ src/controllers/CountScoreController.hs
@@ -12,9 +12,21 @@ getCell f pos = [cell | cell <- (cells f), (position cell) == pos]!!0
 getCenter :: Cell -> Integer
 getCenter c = center $ lines c
 
+getRight :: Cell -> Integer
+getRight c = right $ lines c
+
+getLeft :: Cell -> Integer
+getLeft c = left $ lines c
+
 -- Next three functions return position of the next cell in each direction
 getNextCellDown :: Cell -> Integer
 getNextCellDown c = b $ neighbors c
+
+getNextCellRightBottom :: Cell -> Integer
+getNextCellRightBottom c = rb $ neighbors c
+
+getNextCellLeftBottom :: Cell -> Integer
+getNextCellLeftBottom c = lb $ neighbors c
 
 -- Score counting algorithms
 -- From top to bottom
@@ -25,6 +37,20 @@ countDown f center acc pos  =
   then countDown f 0 0 (-1)
   else countDown f center (acc+1) (getNextCellDown (getCell f pos))
 
+countLeft :: Field -> Integer -> Integer -> Integer -> Integer
+countLeft _ left acc (-1) = left * acc
+countLeft f left acc pos = 
+  if getLeft (getCell f pos) /= left
+  then countLeft f 0 0 (-1)
+  else countLeft f left (acc + 1) (getNextCellLeftBottom $ getCell f pos)
+  
+countRight :: Field -> Integer -> Integer -> Integer -> Integer
+countRight _ right acc (-1) = right * acc
+countRight f right acc pos = 
+  if getRight (getCell f pos) /= right
+  then countRight f 0 0 (-1)
+  else countRight f right (acc + 1) (getNextCellRightBottom $ getCell f pos)
+
 -- Addition of all countings
 countScoreCenter :: Field -> Integer
 countScoreCenter f =
@@ -34,7 +60,25 @@ countScoreCenter f =
   + countDown f (getCenter $ getCell f 4) 0 4
   + countDown f (getCenter $ getCell f 5) 0 5
 
+countScoreLeft :: Field-> Integer
+countScoreLeft f =
+  countLeft f (getLeft $ getCell f 0) 0 0
+  + countLeft f (getLeft $ getCell f 4) 0 4
+  + countLeft f (getLeft $ getCell f 5) 0 5
+  + countLeft f (getLeft $ getCell f 10) 0 10
+  + countLeft f (getLeft $ getCell f 15) 0 15
+  
+countScoreRight :: Field -> Integer
+countScoreRight f =
+  countRight f (getRight $ getCell f 0) 0 0
+  + countRight f (getRight $ getCell f 2) 0 2
+  + countRight f (getRight $ getCell f 1) 0 1
+  + countRight f (getRight $ getCell f 6) 0 6
+  + countRight f (getRight $ getCell f 11) 0 11
+
 -- Returning final result
 countCurrentScore :: Field -> Integer
 countCurrentScore f =
   countScoreCenter f
+  + countScoreLeft f
+  + countScoreRight f
